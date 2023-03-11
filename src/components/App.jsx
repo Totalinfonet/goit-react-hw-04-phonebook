1b7a3368e66069c9546b9d7b1eda069e8d54681a
Totalinfonet <totalinfonet@gmail.com>
2023-03-11 20:20:37 +0200

    Added Step1 logic with Input Form and Contacts List

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,59 @@
-export const App = () => {
-  return <div>React homework template</div>;
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, contacts } = this.state;
+    if (name.trim()) {
+      const newContact = {
+        id: nanoid(),
+        name,
+      };
+      this.setState({
+        contacts: [...contacts, newContact],
+        name: '',
+      });
+    }
+  };
+
+  render() {
+    const { contacts, name } = this.state;
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
