b126ec5abc48f2cb184830e23b1f0b7721b1caba
Totalinfonet <totalinfonet@gmail.com>
2023-03-12 11:11:38 +0200

    ContactForm Component added

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,48 @@
+import { Component } from 'react';
+
+export class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number } = this.state;
+    this.props.onSubmit(name, number);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="text"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
