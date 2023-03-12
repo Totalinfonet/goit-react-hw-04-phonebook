376f30fd3fd9fa0be4168a98c5db6a395adfa63d
Totalinfonet <totalinfonet@gmail.com>
2023-03-12 11:52:49 +0200

    ContactItem Component added

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,14 @@
+import { ContactItem } from '../ContactItem/ContactItem';
+
 export const ContactList = ({ contacts }) => {
   return (
     <ul>
       {contacts.map(contact => (
-        <li key={contact.id}>
-          {contact.name}: {contact.number}
-        </li>
+        <ContactItem
+          key={contact.id}
+          name={contact.name}
+          number={contact.number}
+        />
       ))}
     </ul>
   );
